52b86b92ff590d9097d6e46e8c94bdb35b1604e1
Bruno Henrique Rodrigues <41806574+brunohr@users.noreply.github.com>
2024-08-06 17:59:18 -0300

    Update analiseImg.py

--- analiseImg.py
+++ analiseImg.py
@@ -1,6 +1,19 @@
 import pandas as pd
 from deepface import DeepFace
-#
+from os import listdir
+from os.path import isfile, join
+from datetime import datetime
+
+import pymysql
+
+### INICIANDO CONEXÃO COM O BANCO DE DADOS
+global mydb, cursor
+mydb = pymysql.connect(host="localhost", database='scraping2', user="root", passwd="", port = 3307,
+                       cursorclass=pymysql.cursors.DictCursor)
+cursor = mydb.cursor()
+
+
+
 # def pegaImagem(url):
 #     headOption = webdriver.FirefoxOptions()
 #     headOption.add_argument("--headless")
@@ -28,25 +41,52 @@ def processamentoImgs(url):
         'yunet',
         'centerface',
     ]
+    auxResults = []
     results = []
-    for i in url:
-        print(count)
-        img = 'results/profile_img/'+i
-        if img == "VERIFICAR":
-            results.append("VERIFICAR")
-        elif "amazon-avatars-global/default._" not in img:
-            results.append(DeepFace.analyze(
+    for i in range(0, len(profiles_img)):
+        start = datetime.now()
+        # print(count)
+
+        if 'amazon' in profiles_img[i]:
+            auxId = "amzn1.account." + profiles_img[i].split("_")[1].split(".")[0]
+        elif 'iherb' in profiles_img[i]:
+            auxId = profiles_img[i].split("_")[1]
+        try:
+            cursor.execute("SELECT usuario_id FROM usuario WHERE codigo_perfil = %s;",
+                           (auxId.replace(".png", "").replace(".jpg", "").replace(".jpeg", "")))
+            auxId = cursor.fetchone()['usuario_id']
+        except TypeError:
+            continue
+
+        if cursor.execute("SELECT * FROM resultadoimg WHERE usuario_id = %s", (auxId)):
+            continue
+
+        img = 'results/profile_img/'+profiles_img[i]
+        try:
+            aux = DeepFace.analyze(
                 img_path=img,
-                actions=['gender', 'age', 'race', 'emotion'],
+                # actions=['gender', 'age', 'race', 'emotion'],
+                actions=['gender', 'age', 'race'],
                 enforce_detection=False,
-                detector_backend=backends[7],
-            ))
-        else:
-            results.append("SEM IMAGEM")
+                detector_backend=backends[4],
+            )
+            auxResults.append(aux)
+            if len(aux):
+                try:
+                    results.append([profiles_img[i], aux[0]['dominant_gender'], aux[0]['age'], aux[0]['dominant_race'], auxId])
+                    cursor.execute("INSERT INTO resultadoimg (sexo, idade, raca, usuario_id) VALUES (%s, %s, %s, %s);", results[i][1:])
+                    mydb.commit()
+                except pymysql.Error as e:
+                    print(e)
+                    mydb.rollback()
+        except ValueError:
+            print(print(str(count) + ' VALUE ERROR ' + str(datetime.now() - start)))
+        #
+        print(str(count) + ' ' + str(datetime.now() - start))
+
         count+=1
 
-from os import listdir
-from os.path import isfile, join
 profiles_img = [f for f in listdir('results/profile_img') if isfile(join('results/profile_img', f))]
-
 resultadosImg = processamentoImgs(profiles_img)
+
+
